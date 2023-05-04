e1d59bdd58e85298b665e2d65236ecfe66e189f5
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-05-04 09:56:08 +0300

    Uow and repository design pattern added.

--- DesignPatternsExamples.Repository-UOW/Controllers/TodoItemsController.cs
+++ DesignPatternsExamples.Repository-UOW/Controllers/TodoItemsController.cs
@@ -1,11 +1,71 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DesignPatternsExamples.Repository_UOW.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DesignPatternsExamples.Repository_UOW.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class TodoItemController : ControllerBase
+    [Route("api/[controller]")]
+    public class TodoItemsController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TodoItemsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> GetById(int id)
+        {
+            var todoItem = _unitOfWork.GetRepository<TodoItem>().GetById(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> Create(TodoItem todoItem)
+        {
+            _unitOfWork.GetRepository<TodoItem>().Add(todoItem);
+            _unitOfWork.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = todoItem.Id }, todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.GetRepository<TodoItem>().Update(todoItem);
+            _unitOfWork.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todoItem = _unitOfWork.GetRepository<TodoItem>().GetById(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.GetRepository<TodoItem>().Remove(todoItem);
+            _unitOfWork.SaveChanges();
+
+            return NoContent();
+        }
     }
+
 }
--- DesignPatternsExamples.Repository-UOW/Services/UnitOfWork.cs
+++ DesignPatternsExamples.Repository-UOW/Services/UnitOfWork.cs
@@ -1,6 +1,39 @@
 ﻿namespace DesignPatternsExamples.Repository_UOW.Services
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _context;
+        private Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            if (_repositories == null)
+                _repositories = new Dictionary<Type, object>();
+
+            var type = typeof(TEntity);
+            if (!_repositories.ContainsKey(type))
+                _repositories[type] = new Repository<TEntity>(_context);
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
